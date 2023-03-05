4a28d423c66e8249315a4eb56561840cd48e903e
welton <wangwei_omg@163.com>
2023-03-05 10:46:40 +0800

    rm useless my page funcs

--- miniprogram/pages/navigator/my/index.js
+++ miniprogram/pages/navigator/my/index.js
@@ -14,14 +14,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    wx.cloud.callFunction({
-      name: 'bill',
-      data: {
-        type: 'getCardList'
-      },
-    }).then( res=> {
-      console.log(res)
-    }).catch(console.error)
 
   },
 
