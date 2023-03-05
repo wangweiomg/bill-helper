699de32829f8614da2654bf20b562d569b9a23b3
welton <wangwei_omg@163.com>
2023-03-05 21:09:19 +0800

    add category of bill

---
+++ miniprogram/components/content-card/index.js
@@ -0,0 +1,66 @@
+// components/content-card/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name: {
+      type: String
+    },
+    cardPadding: {
+      type: Boolean,
+      value: true
+    },
+    isShowAddIcon: {
+      type: Boolean,
+      value: true
+    },
+    desc: {
+      type: String
+    },
+
+    businessId: {
+      type: String
+    }
+
+  
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    clickAddIcon(e) {
+    
+      let navigateTo = ''
+      switch (this.properties.businessId) {
+        case "credit_card":
+          navigateTo = "/pages/form/card/index";
+          break;
+        case "house_loan":
+          navigateTo = "/pages/form/house/index";
+          break;
+        case "promissory_note":
+          navigateTo = "/pages/form/promissory/index";
+          break;
+        case "iou":
+          navigateTo = "/pages/form/iou/index";
+          break;
+      }
+      wx.navigateTo({
+        url: navigateTo,
+      })
+
+
+    }
+
+  }
+})
